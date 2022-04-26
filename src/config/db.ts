c4baa865e9ef28ce4cbf5571e66cdbc7eb4f77e7
Francisco Messina <francisco@messina.ws>
2022-04-25 23:18:57 -0300

    added dotenv to use for env variables

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,10 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDb = async () => {
   try {
-    await mongoose.connect(
-      'mongodb://fran:coderhouse2022@cluster1-shard-00-00.zzoqs.mongodb.net:27017,cluster1-shard-00-01.zzoqs.mongodb.net:27017,cluster1-shard-00-02.zzoqs.mongodb.net:27017/test?replicaSet=atlas-wwir4a-shard-0&ssl=true&authSource=admin',
-      {}
-    );
+    await mongoose.connect(process.env.MONGO_URI!, {});
   } catch (error) {
     console.error(error);
   }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 import { createApp } from './config/express';
 import http from 'http';
 import { connectDb } from './config/db';
+import dotenv from 'dotenv';
 
-import admin from 'firebase-admin';
-import key from '../firebase-key.json';
-
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
   connectDb().catch((err) => console.log(err));
-
 
   const app = await createApp();
 
